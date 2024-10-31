c63810d29de21a2696c93020827925e913f865a7
Gabriel Santos <Gabriel Santos@DESKTOP-L9OH9C4>
2024-10-31 06:31:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var windows = base.CreateWindow(activationState);
+
+            windows.Width = 400;
+            windows.Height = 600;
+
+            return windows;
+           
+        }
+    }
+}
